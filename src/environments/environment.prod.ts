957ae418912a2e245ebe37ac3fc662e1873e91d0
dhruv rathod <dhruvthenorthstar@gmail.com>
2019-03-15 09:08:50 -0400

    environment  db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAIhWA7slUCPkpswSZCqkC1YPKuD52NEus',
+    authDomain: 'clientpanelprod-f75a3.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-f75a3.firebaseio.com',
+    projectId: 'clientpanelprod-f75a3',
+    storageBucket: 'clientpanelprod-f75a3.appspot.com',
+    messagingSenderId: '9848501901'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAIhWA7slUCPkpswSZCqkC1YPKuD52NEus',
+    authDomain: 'clientpanelprod-f75a3.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-f75a3.firebaseio.com',
+    projectId: 'clientpanelprod-f75a3',
+    storageBucket: 'clientpanelprod-f75a3.appspot.com',
+    messagingSenderId: '9848501901'
+  }
 };
 
 /*
